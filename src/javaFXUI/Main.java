ff1ba653a0f2dacecfe49565b8e7ea0f88702279
Aleksandr Tarasenko <work.tarasenko@gmail.com>
2015-02-19 15:30:39 +0200

    Fixed imports warnings at Main.java

--- src/javaFXUI/Main.java
+++ src/javaFXUI/Main.java
@@ -1,23 +1,10 @@
 package javaFXUI;
-import YourMoneySaver.*;
-
-import com.sun.javafx.geom.Rectangle;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
