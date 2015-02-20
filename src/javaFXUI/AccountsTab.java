7fda6953c9b60c48fc72a042b5b4de36eaf903e0
alekstar <inet.tarasenko@gmail.com>
2015-02-20 14:43:35 +0200

    Added buttons for add and delete accounts

--- src/javaFXUI/AccountsTab.java
+++ src/javaFXUI/AccountsTab.java
@@ -1,17 +1,37 @@
 package javaFXUI;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class AccountsTab {
 	
 	public Tab getTab() {
 	    Tab accounts = new Tab();
-		accounts.setText("Accounts");
+		accounts.setText(getAccountsTabText());
 		accounts.setClosable(false);
-		VBox gridPane = new VBox();
+		VBox vBox = new VBox();
 		AccountsTable accountsTable = new AccountsTable();
-		gridPane.getChildren().add(accountsTable.getTable());
-		accounts.setContent(gridPane);
+		vBox.getChildren().add(accountsTable.getTable());
+		HBox accountOperationsToolBox = getAccountOperationsToolBox();
+		vBox.getChildren().add(accountOperationsToolBox);
+		vBox.setPadding(getVBoxInsideOffsets());
+		accounts.setContent(vBox);
 	    return accounts;
+    }
+
+	private Insets getVBoxInsideOffsets() {
+	    return new Insets(10, 10, 10, 10);
+    }
+
+	private String getAccountsTabText() {
+	    return "Accounts";
+    }
+
+	private HBox getAccountOperationsToolBox() {
+	    AccountOperationsToolBox accountOperationsToolBox = 
+	    		new AccountOperationsToolBox();
+	    return accountOperationsToolBox.getHBox();
     }
 }
