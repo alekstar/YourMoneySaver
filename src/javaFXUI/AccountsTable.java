67442d86321887adc442fce3843daf396a94f327
alekstar <inet.tarasenko@gmail.com>
2015-02-20 10:48:07 +0200

    Refactored AccountsTable.java

--- src/javaFXUI/AccountsTable.java
+++ src/javaFXUI/AccountsTable.java
@@ -11,48 +11,76 @@ import YourMoneySaver.Currency;
 import YourMoneySaver.Money;
 
 public class AccountsTable {
-	private final ObservableList<Account> accounts = 
-			FXCollections.observableArrayList();
-	
+	private final ObservableList<Account> accounts = FXCollections
+	        .observableArrayList();
+
 	private void fillAccountsWithTestData() {
 		Currency uah = new Currency("Украинская гривня", "UAH");
 		AccountType plasticCard = new AccountType("Пластиковая карта");
-		this.accounts.add(
-				new Account(
-						"Карта ПриватБанка", 
-						plasticCard, 
-						new Money(uah, 150, 45), 
-						"Выдана 19.02.2015"));
+		this.accounts.add(new Account("Карта ПриватБанка", plasticCard,
+		        new Money(uah, 150, 45), "Выдана 19.02.2015"));
 	}
-	
+
+	private String getAccountCommentsFieldName() {
+		return "comments";
+	}
+
+	private String getAccountNameFieldName() {
+		return "name";
+	}
+
+	private String getAccountRestFieldName() {
+		return "rest";
+	}
+
+	private String getAccountTypeFieldName() {
+		return "accountType";
+	}
+
+	private String getColumnNameForAccountComments() {
+		return "Comments";
+	}
+
+	private String getColumnNameForAccountName() {
+		return "Name";
+	}
+
+	private String getColumnNameForAccountRest() {
+		return "Rest";
+	}
+
+	private String getColumnNameForAccountType() {
+		return "Type";
+	}
+
 	public TableView<Account> getTable() {
 		TableView<Account> tableView = new TableView<Account>();
-		
-		TableColumn<Account, String> name = 
-				new TableColumn<Account, String>("Name");
-		name.setCellValueFactory(
-				new PropertyValueFactory<Account, String>("name"));
-		TableColumn<Account, String> type = 
-				new TableColumn<Account, String>("Type");
-		type.setCellValueFactory(
-				new PropertyValueFactory<Account, String>("accountType"));
-		TableColumn<Account, String> rest = 
-				new TableColumn<Account, String>("Rest");
-		rest.setCellValueFactory(
-				new PropertyValueFactory<Account, String>("rest"));
-		TableColumn<Account, String> comments = 
-				new TableColumn<Account, String>("Comments");
-		comments.setCellValueFactory(
-				new PropertyValueFactory<Account, String>("comments"));
+
+		TableColumn<Account, String> name = new TableColumn<Account, String>(
+				getColumnNameForAccountName());
+		name.setCellValueFactory(new PropertyValueFactory<Account, String>(
+		        getAccountNameFieldName()));
+		TableColumn<Account, String> type = new TableColumn<Account, String>(
+		        getColumnNameForAccountType());
+		type.setCellValueFactory(new PropertyValueFactory<Account, String>(
+		        getAccountTypeFieldName()));
+		TableColumn<Account, String> rest = new TableColumn<Account, String>(
+		        getColumnNameForAccountRest());
+		rest.setCellValueFactory(new PropertyValueFactory<Account, String>(
+		        getAccountRestFieldName()));
+		TableColumn<Account, String> comments = new TableColumn<Account, String>(
+		        getColumnNameForAccountComments());
+		comments.setCellValueFactory(new PropertyValueFactory<Account, String>(
+		        getAccountCommentsFieldName()));
 
 		fillAccountsWithTestData();
-		
+
 		tableView.getColumns().add(name);
 		tableView.getColumns().add(type);
 		tableView.getColumns().add(rest);
 		tableView.getColumns().add(comments);
 		tableView.setItems(this.accounts);
-		
+
 		return tableView;
 	}
 }
