7b6d007928d5b1dfeac14b6e6f12ecef36906620
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-02-22 12:10:15 +0200

    Sorted members of Account class

--- src/YourMoneySaver/Account.java
+++ src/YourMoneySaver/Account.java
@@ -6,19 +6,28 @@ public class Account {
 	String name;
 	String comments;
 
-	public Money getRest() {
-		return rest;
-	}
-
-	public void setRest(Money rest) throws IllegalArgumentException {
-		if(rest == null) {
-			throw new IllegalArgumentException("Argument rest is null.");
-		}
-		this.rest = rest;
-	}
+	public Account(String name, AccountType accountType, Money rest,
+            String comments) throws IllegalArgumentException {
+		setRest(rest);
+		setAccountType(accountType);
+		setName(name);
+		setComments(comments);
+    }
 
 	public AccountType getAccountType() {
 		return accountType;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Money getRest() {
+		return rest;
 	}
 
 	public void setAccountType(AccountType accountType) {
@@ -29,29 +38,20 @@ public class Account {
 		this.accountType = accountType;
 	}
 
-	public String getName() {
-		return name;
+	public void setComments(String comments) {
+		this.comments = comments;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getComments() {
-		return comments;
+	public void setRest(Money rest) throws IllegalArgumentException {
+		if(rest == null) {
+			throw new IllegalArgumentException("Argument rest is null.");
+		}
+		this.rest = rest;
 	}
-
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
-
-	public Account(String name, AccountType accountType, Money rest,
-            String comments) throws IllegalArgumentException {
-		setRest(rest);
-		setAccountType(accountType);
-		setName(name);
-		setComments(comments);
-    }
 	
 	
 }
