16e2c2452e338a42c419a910752e7675e2aa4448
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-02-22 12:13:09 +0200

    Sorted members of Currency class

--- src/YourMoneySaver/Currency.java
+++ src/YourMoneySaver/Currency.java
@@ -4,33 +4,33 @@ public class Currency {
 	private String isoCode;
 	private String sign;
 	
-	public String getSign() {
-		return sign;
-	}
-
-	private void setSign(String sign) {
-		this.sign = sign;
+	public Currency(String name, String isoCode, String sign) {
+		setName(name);
+		setIsoCode(isoCode);
+		setSign(sign);
 	}
 
 	public String getIsoCode() {
 		return isoCode;
 	}
-	
-	private void setIsoCode(String iSOCode) {
-		isoCode = iSOCode;
-	}
 
 	public String getName() {
 		return name;
+	}
+	
+	public String getSign() {
+		return sign;
+	}
+
+	private void setIsoCode(String iSOCode) {
+		isoCode = iSOCode;
 	}
 
 	private void setName(String name) {
 		this.name = name;
 	}
 
-	public Currency(String name, String isoCode, String sign) {
-		setName(name);
-		setIsoCode(isoCode);
-		setSign(sign);
+	private void setSign(String sign) {
+		this.sign = sign;
 	}
 }
