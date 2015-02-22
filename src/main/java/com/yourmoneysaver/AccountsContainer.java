3f18a6197970550f8d042d1d545c3f187f78e4f3
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-02-22 22:27:18 +0200

    Added AccountsContainer class

---
+++ src/main/java/com/yourmoneysaver/AccountsContainer.java
@@ -0,0 +1,29 @@
+package com.yourmoneysaver;
+
+import java.util.List;
+
+public class AccountsContainer {
+	List<Account> accounts;
+	
+	public int add(Account account){
+		verifyAccounts();
+		accounts.add(account);
+		return (accounts.size() - 1);
+	}
+	
+	public Account get(int index) {
+		verifyAccounts();
+		return accounts.get(index);
+	}
+
+	private void verifyAccounts() {
+		if(accounts == null) {
+			throw new IllegalStateException("Accounts container is not initialized.");
+		}
+	}
+	
+	public void remove(int index) {
+		verifyAccounts();
+		accounts.remove(index);
+	}
+}
