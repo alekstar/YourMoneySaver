275e31abb3fe494ac2609bff2538459dcd067824
alekstar <inet.tarasenko@gmail.com>
2015-02-23 17:00:54 +0200

    Added few controls to Add Account Window

--- src/main/java/com/yourmoneysaver/javafxui/AddAccountWindow.java
+++ src/main/java/com/yourmoneysaver/javafxui/AddAccountWindow.java
@@ -1,27 +1,69 @@
 package com.yourmoneysaver.javafxui;
 
+
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class AddAccountWindow {
-	Stage parentWindow;
-	Stage thisWindow;
+	private Stage parentWindow;
+	private Stage thisWindow;
+	private StackPane rootPane;
 
 	public AddAccountWindow(Stage parentWindow) {
 		setParentWindow(parentWindow);
 		initializeThisWindow();
+		addControlsToThisWindow();
+	}
+
+	public StackPane getRootPane() {
+		return rootPane;
+	}
+
+	private void setRootPane(StackPane rootPane) {
+		if(rootPane == null) {
+			throw new IllegalArgumentException("Argument rootPane is null.");
+		}
+		this.rootPane = rootPane;
 	}
 
 	private void initializeThisWindow() {
-		Scene mainWindow = new Scene(new StackPane(), getWidth(), getHeight());
-		Stage stage = new Stage();
-		stage.setScene(mainWindow);
-		stage.initOwner(getParentWindow());
-		stage.initModality(Modality.WINDOW_MODAL);
-		stage.setResizable(false);
-		setThisWindow(stage);
+		setRootPane(new StackPane());
+		setThisWindow(new Stage());
+		Scene mainWindow = new Scene(getRootPane(), getWidth(), getHeight());
+		getThisWindow().setScene(mainWindow);
+		getThisWindow().initOwner(getParentWindow());
+		getThisWindow().initModality(Modality.WINDOW_MODAL);
+		getThisWindow().setResizable(false);
+	}
+	
+	private Label getLabelInstance(String labelText) {
+		return new Label(labelText);
+	}
+	
+	private Label getNameFieldLabel() {
+		return getLabelInstance(getNameFieldLabelText());
+	}
+
+	private String getNameFieldLabelText() {
+		return "Name";
+	}
+	
+	private TextField getNameTextField() {
+		return new TextField(getNameFieldLabelText());
+	}
+	
+	private void addControlsToThisWindow() {
+		VBox mainPane = new VBox();
+		mainPane.getChildren().add(getNameFieldLabel());
+		mainPane.getChildren().add(getNameTextField());
+		mainPane.setPadding(getMainPanePadding());
+		getRootPane().getChildren().add(mainPane);
 	}
 
 	private int getHeight() {
@@ -36,7 +78,7 @@ public class AddAccountWindow {
 		return thisWindow;
 	}
 
-	public void setThisWindow(Stage thisWindow) {
+	private void setThisWindow(Stage thisWindow) {
 		if(thisWindow == null) {
 			throw new IllegalArgumentException("Argument thisWindow is null.");
 		}
@@ -47,7 +89,7 @@ public class AddAccountWindow {
 		return parentWindow;
 	}
 
-	public void setParentWindow(Stage parentWindow) {
+	private void setParentWindow(Stage parentWindow) {
 		if(parentWindow == null) {
 			throw new IllegalArgumentException(
 					"Argument parentWindow is null.");
@@ -58,4 +100,8 @@ public class AddAccountWindow {
 	public void showAndWait() {
 		getThisWindow().showAndWait();
 	}
+	
+	private Insets getMainPanePadding() {
+	    return new Insets(10, 10, 10, 10);
+    }
 }
