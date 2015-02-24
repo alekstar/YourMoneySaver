54d9d136493ca10a0be968618605e3f00d4eb4bb
alekstar <inet.tarasenko@gmail.com>
2015-02-24 12:26:13 +0200

    added exception class ArgumentIsNullException that will be thrown while unexcpected null argument is passed

---
+++ src/main/java/com/yourmoneysaver/exceptions/ArgumentIsNullException.java
@@ -0,0 +1,13 @@
+package com.yourmoneysaver.exceptions;
+
+public class ArgumentIsNullException extends IllegalArgumentException {
+	public ArgumentIsNullException(String argumentName) {
+		/* had to make this crappy code because constructor of superclass 
+	     * have to be called first */
+		super((new StringBuilder())
+				.append("Argument \"")
+				.append(argumentName)
+				.append("\" is null.")
+				.toString());
+	}
+}
