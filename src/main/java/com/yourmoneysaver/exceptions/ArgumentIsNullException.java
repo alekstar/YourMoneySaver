3264cfea083948dac5d15310ffb4e9402c0a2795
alekstar <inet.tarasenko@gmail.com>
2015-02-24 14:50:57 +0200

    added serialVersionUID to class ArgumentIsNullException

--- src/main/java/com/yourmoneysaver/exceptions/ArgumentIsNullException.java
+++ src/main/java/com/yourmoneysaver/exceptions/ArgumentIsNullException.java
@@ -1,6 +1,8 @@
 package com.yourmoneysaver.exceptions;
 
 public class ArgumentIsNullException extends IllegalArgumentException {
+    private static final long serialVersionUID = 1L;
+
     public ArgumentIsNullException(String argumentName) {
         /*
          * had to make this crappy code because constructor of superclass have
