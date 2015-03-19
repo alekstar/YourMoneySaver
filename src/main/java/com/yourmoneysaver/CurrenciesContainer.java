5207c9b967ba2dddb39cc9073827a1bba4b0f7fe
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-03-19 12:20:13 +0200

    added CurrenciesContainer class

---
+++ src/main/java/com/yourmoneysaver/CurrenciesContainer.java
@@ -0,0 +1,44 @@
+package com.yourmoneysaver;
+
+import java.util.TreeSet;
+
+import com.yourmoneysaver.exceptions.ArgumentIsNullException;
+
+public class CurrenciesContainer {
+    TreeSet<Currency> currencies;
+
+    protected CurrenciesContainer() {
+        setCurrencies(new TreeSet<Currency>());
+    }
+
+    public static CurrenciesContainer create() {
+        return new CurrenciesContainer();
+    }
+
+    private void setCurrencies(TreeSet<Currency> currencies) {
+        if (currencies == null) {
+            throw new ArgumentIsNullException("currencies");
+        }
+        this.currencies = currencies;
+    }
+
+    public void add(Currency currency) {
+        verifyCurrencies();
+        currencies.add(currency);
+    }
+
+    private void verifyCurrencies() {
+        if (currencies == null) {
+            throw new IllegalStateException(
+                    "Reference to list of currencies is null.");
+        }
+    }
+
+    public TreeSet<Currency> getCurrencies() {
+        return this.currencies;
+    }
+
+    public void delete(Currency currency) {
+        currencies.remove(currency);
+    }
+}
