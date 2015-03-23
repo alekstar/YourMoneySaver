ee6ac73cbc568b5e5da6476f85c665e03a3b9dff
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-03-23 16:40:41 +0200

    added shouldThrowExceptionWhenNameIsNull case

--- src/main/java/com/alekstar/yourmoneysaver/CurrencyTest.java
+++ src/main/java/com/alekstar/yourmoneysaver/CurrencyTest.java
@@ -1,8 +1,11 @@
 package com.alekstar.yourmoneysaver;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
+
+import com.alekstar.yourmoneysaver.exceptions.ArgumentIsNullException;
 
 public class CurrencyTest {
 
@@ -28,5 +31,12 @@ public class CurrencyTest {
     public void shouldCreateCurrencyObjectWithEmptyComments() {
         Currency uah = new Currency("Ukrainian hrivnya", "UAH", "₴", null);
         assertEquals(null, uah.getComments());
+    }
+
+    @Test(expected = ArgumentIsNullException.class)
+    public void shouldThrowExceptionWhenNameIsNull() {
+        @SuppressWarnings("unused")
+        Currency uah = new Currency(null, "UAH", "₴", null);
+        fail();
     }
 }
