aa54f2a7b798f8f37e9d50ce3179e4d53c5ae274
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-04-27 11:31:48 +0300

    CurrenciesDataAccessObject now works with CurrencyEntity interface

--- src/main/java/com/alekstar/yourmoneysaver/CurrenciesDataAccessObject.java
+++ src/main/java/com/alekstar/yourmoneysaver/CurrenciesDataAccessObject.java
@@ -2,10 +2,12 @@ package com.alekstar.yourmoneysaver;
 
 import java.util.List;
 
+import com.alekstar.yourmoneysaver.database.CurrencyEntity;
+
 public interface CurrenciesDataAccessObject {
-    void save(Currency currency);
+    void save(CurrencyEntity currency);
 
-    void remove(Currency currency);
+    void remove(CurrencyEntity currency);
 
-    List<Currency> loadAll();
+    List<CurrencyEntity> loadAll();
 }
