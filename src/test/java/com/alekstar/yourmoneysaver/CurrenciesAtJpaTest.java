3e6fe98b473840b0d157176c9907dc3d0c69ac56
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-04-28 15:09:32 +0300

    added CurrenciesAtJpaTest tests class
    and so tested creating US Dollar currency

---
+++ src/test/java/com/alekstar/yourmoneysaver/CurrenciesAtJpaTest.java
@@ -0,0 +1,65 @@
+package com.alekstar.yourmoneysaver;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.Test;
+
+import com.alekstar.yourmoneysaver.database.CurrenciesAtJPA;
+import com.alekstar.yourmoneysaver.database.CurrencyEntity;
+import com.alekstar.yourmoneysaver.database.CurrencyEntityAtJPA;
+
+public class CurrenciesAtJpaTest {
+    EntityManagerFactory entityManagerFactory = Persistence
+            .createEntityManagerFactory(defineNameOfPersistenceUnit());
+
+    private String defineNameOfPersistenceUnit() {
+        return "YourMoneySaverH2TestDataBase";
+    }
+
+    private CurrencyEntity createUSDCurrencyToDataBase() {
+        EntityManager entityManager =
+                entityManagerFactory.createEntityManager();
+        Currency usd = new Currency("United States dollar", "USD", "$", null);
+        CurrencyEntity usdEntity = new CurrencyEntityAtJPA(usd);
+        try {
+            CurrenciesDataAccessObject currenciesDataAccessObject =
+                    CurrenciesAtJPA.create(entityManager);
+            currenciesDataAccessObject.save(usdEntity);
+            return usdEntity;
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    private CurrencyEntity readFirstCurrencyFromDataBase() {
+        EntityManager entityManager =
+                entityManagerFactory.createEntityManager();
+        try {
+            CurrenciesDataAccessObject currenciesDataAccessObject =
+                    CurrenciesAtJPA.create(entityManager);
+
+            List<CurrencyEntity> listOfCurrenciesEntities =
+                    currenciesDataAccessObject.loadAll();
+            return listOfCurrenciesEntities.get(0);
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    @Test
+    public void shouldCreateUSDCurrency() {
+        try {
+            CurrencyEntity expected = createUSDCurrencyToDataBase();
+            CurrencyEntity actual = readFirstCurrencyFromDataBase();
+            assertEquals(expected, actual);
+        } finally {
+            this.entityManagerFactory.close();
+        }
+    }
+}
