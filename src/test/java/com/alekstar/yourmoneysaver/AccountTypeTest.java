615094957e0808fa55abc8f98046b398c018b38a
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-06-08 11:42:31 +0300

    added shouldThrowArgumentIsNullExceptionIfNameIsNullAndCommentsIsNull test case for AccountType class

--- src/test/java/com/alekstar/yourmoneysaver/AccountTypeTest.java
+++ src/test/java/com/alekstar/yourmoneysaver/AccountTypeTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.alekstar.yourmoneysaver.exceptions.ArgumentIsNullException;
+
 public class AccountTypeTest {
 
     @Test
@@ -11,5 +13,12 @@ public class AccountTypeTest {
         String expectedName = "Nice Account Type";
         AccountType accountType = new AccountType(expectedName, null);
         assertEquals(expectedName, accountType.getName());
+    }
+
+    @Test(expected = ArgumentIsNullException.class)
+    public void shouldThrowArgumentIsNullExceptionIfNameIsNullAndCommentsIsNull() {
+        @SuppressWarnings("unused")
+        AccountType accountType = new AccountType(null, null);
+        fail();
     }
 }
