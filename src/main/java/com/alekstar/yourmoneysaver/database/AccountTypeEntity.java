745707db35899f4317ddf88ed808cbb77f89c73a
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-06-08 15:10:12 +0300

    added AccountTypeEntity interface

---
+++ src/main/java/com/alekstar/yourmoneysaver/database/AccountTypeEntity.java
@@ -0,0 +1,11 @@
+package com.alekstar.yourmoneysaver.database;
+
+public interface AccountTypeEntity {
+    void setName(String name);
+
+    String getName();
+
+    void setComments(String comments);
+
+    String getComments();
+}
