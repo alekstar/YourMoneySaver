01a53af0e489d57b3b328a6a0e132d665d64c9db
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-06-12 14:38:08 +0300

    added shouldBe2Dot00 test case

--- src/test/java/com/alekstar/yourmoneysaver/BigDecimal/BigDecimalTest.java
+++ src/test/java/com/alekstar/yourmoneysaver/BigDecimal/BigDecimalTest.java
@@ -3,6 +3,11 @@ package com.alekstar.yourmoneysaver.BigDecimal;
 import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 import org.junit.Test;
 
@@ -11,5 +16,39 @@ public class BigDecimalTest {
     public void shouldBe2Dot34() {
         BigDecimal first = new BigDecimal("2.34");
         assertEquals("2.34", first.toString());
+    }
+
+    @Test
+    public void shouldBe2Dot00() {
+        BigDecimal first =
+                new BigDecimal("2", new MathContext(2, RoundingMode.HALF_UP));
+        String result = defineStringInFormat(first, 2);
+        String expected = defineExpectedValueForShouldBe2Dot00TestCase();
+        assertEquals(expected, result);
+    }
+
+    private String defineExpectedValueForShouldBe2Dot00TestCase() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append('2');
+        stringBuilder.append(defineDecimalSeparator());
+        stringBuilder.append('0');
+        stringBuilder.append('0');
+        String expected = stringBuilder.toString();
+        return expected;
+    }
+
+    private char defineDecimalSeparator() {
+        DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols();
+        return decimalFormatSymbols.getDecimalSeparator();
+    }
+
+    private String defineStringInFormat(BigDecimal bigDecimal,
+            int digitsAfterSeparator) {
+        DecimalFormat decimalFormat = new DecimalFormat();
+        decimalFormat.setMaximumFractionDigits(digitsAfterSeparator);
+        decimalFormat.setMinimumFractionDigits(digitsAfterSeparator);
+        decimalFormat.setGroupingUsed(false);
+        String result = decimalFormat.format(bigDecimal);
+        return result;
     }
 }
