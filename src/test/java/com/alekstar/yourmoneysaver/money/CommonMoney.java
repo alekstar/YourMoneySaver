f47fec767f88eed6c2843bc59b5450453a12d075
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-06-16 12:48:43 +0300

    created CommonMoney class and added create method to it
    now test case shouldCreateCommonMoneyObject() is green

---
+++ src/test/java/com/alekstar/yourmoneysaver/money/CommonMoney.java
@@ -0,0 +1,10 @@
+package com.alekstar.yourmoneysaver.money;
+
+import com.alekstar.yourmoneysaver.Currency;
+
+public class CommonMoney implements Money {
+
+    public static Money create(String decimalPart, Currency currency) {
+        return null;
+    }
+}
