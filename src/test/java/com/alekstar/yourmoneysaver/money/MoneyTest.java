83f9a2179549ff07526eacad41f4cd2f4f44081d
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-06-16 12:49:54 +0300

    cleaned up MoneyTest.java

--- src/test/java/com/alekstar/yourmoneysaver/money/MoneyTest.java
+++ src/test/java/com/alekstar/yourmoneysaver/money/MoneyTest.java
@@ -1,9 +1,5 @@
 package com.alekstar.yourmoneysaver.money;
 
-import static org.junit.Assert.*;
-
-import java.math.BigDecimal;
-
 import org.junit.Test;
 
 import com.alekstar.yourmoneysaver.Currency;
@@ -12,6 +8,7 @@ public class MoneyTest {
     @Test
     public void shouldCreateCommonMoneyObject() {
         Currency usd = new Currency("US Dollar", "USD", "$", null);
+        @SuppressWarnings("unused")
         Money money = CommonMoney.create("45.78", usd);
     }
 }
