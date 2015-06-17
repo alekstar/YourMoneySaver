8c84ea946b736f3e49ecdf9c329dabb107a48c5c
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-06-17 12:26:33 +0300

    extracted method createCommonMoneyFor45Point78USD() in CommonMoneyTest class

--- src/test/java/com/alekstar/yourmoneysaver/money/CommonMoneyTest.java
+++ src/test/java/com/alekstar/yourmoneysaver/money/CommonMoneyTest.java
@@ -12,30 +12,34 @@ public class CommonMoneyTest {
     @Test
     public void shouldCreateCommonMoneyObject() {
         @SuppressWarnings("unused")
-        Money money = CommonMoney.create("45.78", USD);
+        Money money = createCommonMoneyFor45Point78USD();
     }
 
     @Test
     public void shouldCreateNotNullCommonMoneyObject() {
-        Money money = CommonMoney.create("45.78", USD);
+        Money money = createCommonMoneyFor45Point78USD();
         assertNotNull(money);
     }
 
     @Test
     public void shouldHaveGetCurrencyMethod() {
-        Money money = CommonMoney.create("45.78", USD);
+        Money money = createCommonMoneyFor45Point78USD();
         money.getCurrency();
     }
 
     @Test
     public void shouldGetCurrencyReturnCurrencyObject() {
-        Money money = CommonMoney.create("45.78", USD);
+        Money money = createCommonMoneyFor45Point78USD();
         assertTrue(money.getCurrency() instanceof Currency);
     }
 
     @Test
     public void shouldCreateMethodReturnCommonMoneyObject() {
-        Money money = CommonMoney.create("45.78", USD);
+        Money money = createCommonMoneyFor45Point78USD();
         assertTrue(money instanceof CommonMoney);
+    }
+
+    private Money createCommonMoneyFor45Point78USD() {
+        return CommonMoney.create("45.78", USD);
     }
 }
