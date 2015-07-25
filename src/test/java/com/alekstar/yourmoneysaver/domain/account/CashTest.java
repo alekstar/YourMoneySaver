7352eecce65b60192cc2fe51a54e6e9ee083dd9f
Aleksandr Tarasenko <inet.tarasenko@gmail.com>
2015-07-25 19:57:28 +0300

    added haveGetNameReturnPocket test case for Cash class

---
+++ src/test/java/com/alekstar/yourmoneysaver/domain/account/CashTest.java
@@ -0,0 +1,17 @@
+package com.alekstar.yourmoneysaver.domain.account;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.alekstar.yourmoneysaver.domain.Currency;
+
+public class CashTest {
+    Account pocket = new Cash("Pocket", new Currency("US Dollar", "USD", "$",
+            null), null);
+
+    @Test
+    public void haveGetNameReturnPocket() {
+        assertEquals("Pocket", pocket.getName());
+    }
+}
